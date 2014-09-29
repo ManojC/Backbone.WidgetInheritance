3779a596f885f0f3f4e9f045fb112d06854da551
Manoj Chalode <mchalode@tavisca.com>
2014-09-29 20:23:26 +0530

    added basic views and templates. Views are not functional yet.

--- Backbone.WidgetInheritance.UI/Scripts/Views/InfoView.js
+++ Backbone.WidgetInheritance.UI/Scripts/Views/InfoView.js
@@ -1,1 +1,49 @@
-﻿
+﻿(function ($, window, undefined) {
+
+    window.WI = window.WI || {};
+    window.WI.Views = window.WI.Views || {};
+
+    //base info view definition
+    window.WI.Views.InfoView = Backbone.View.extend({
+
+        //default value for element. Expected to be overridden by child view.
+        el: 'body',
+
+        templatePath: '',
+
+        template: '',
+
+        initialize: function () {
+
+            //bind general info events here..
+            this.bindEvents();
+            this.initializeChild();
+        },
+
+        //this can be overridden by child view for custom event handling..
+        initializeChild: function () { },
+
+        render: function () {
+
+            var data = this.getDatFromServer();
+            var html = Handlebars.compile(this.template)(data);
+            this.$el.html(html);
+        },
+
+        events: {
+
+        },
+
+        bindEvents: function () {
+            var that = this;
+            $.get(this.templatePath, function (template) {
+                that.template = template;
+            });
+        },
+        
+        getDatFromServer: function() {
+            
+        }
+    });
+
+})(jQuery, window);
--- Backbone.WidgetInheritance.UI/Scripts/Views/UserDetailsView.js
+++ Backbone.WidgetInheritance.UI/Scripts/Views/UserDetailsView.js
@@ -1,1 +1,40 @@
-﻿
+﻿(function($, window, undefined) {
+
+    window.WI = window.WI || {};
+    window.WI.Views = window.WI.Views || {};
+
+    //base info view definition
+    window.WI.Views.DetailsView = Backbone.View.extend({
+
+        el: '#bodyColumn',
+
+        templatePath: '/Templates/details.html',
+
+        template: '',
+
+        initialize: function () {
+            var that = this;
+            $.get(this.templatePath, function (template) {
+                that.template = template;
+                console.log(template);
+            });
+        },
+
+        render: function () {
+
+            this.renderGeneralInfoView();
+            this.renderGeneralInfoView();
+            this.renderGeneralInfoView();
+
+        },
+
+        events: {
+
+        },
+
+        getDatFromServer: function () {
+
+        }
+    });
+
+})(jQuery, window);
