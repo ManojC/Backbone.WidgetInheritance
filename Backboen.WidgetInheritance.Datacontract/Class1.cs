64467d160baf551aeffa9282c7d7c1d8fd774de5
Manoj Chalode <mchalode@tavisca.com>
2014-09-29 16:51:24 +0530

    added solution to git repo

---
+++ Backboen.WidgetInheritance.Datacontract/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backboen.WidgetInheritance.Datacontract
+{
+    public class Class1
+    {
+    }
+}
